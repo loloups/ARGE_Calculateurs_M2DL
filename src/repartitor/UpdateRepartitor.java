cba9edb6a1cca801207b89b06715ef26533b3e6e
loloups <l.leger47@gmail.com>
2016-05-10 21:32:28 +0000

    Fix bug with rpc network

--- src/repartitor/UpdateRepartitor.java
+++ src/repartitor/UpdateRepartitor.java
@@ -9,12 +9,13 @@ import org.apache.xmlrpc.client.XmlRpcCommonsTransportFactory;
 public class UpdateRepartitor {
 
     public static void main(String[] args) {
-        if (args.length != 4) {
+        if (args.length != 5) {
             System.err.println(
                 "UpdateRepartitor needs exactly 4 parameters to start : operator address, operator port, action, calculator port (if action=add) or id of the worker node (if action=delete).");
         }
         else {
             try {
+		System.out.println("http://" + args[0] + ":"+args[1]+"/xmlrpc");
             	XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
     			config.setServerURL(new URL("http://" + args[0] + ":"+args[1]+"/xmlrpc"));
     			config.setEnabledForExtensions(true);
